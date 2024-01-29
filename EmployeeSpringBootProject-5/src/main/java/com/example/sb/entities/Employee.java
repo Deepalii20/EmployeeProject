718168d56e2ec1f55bd2e8a8f3d2610741c8345d
DeepaliP <DeepaliP@AST-OL-208>
2024-01-29 10:54:46 +0530

    Initial commit

---
+++ EmployeeSpringBootProject-5/src/main/java/com/example/sb/entities/Employee.java
@@ -0,0 +1,43 @@
+package com.example.sb.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	String empId;
+	String name;
+	float salary;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Employee(String empId, String name, float salary) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.salary = salary;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
+
+}
